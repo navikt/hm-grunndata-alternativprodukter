1b82981639b650b2da17545d9205c9b5ebdf8426
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-10-04 08:38:39 +0200

    Made the service more kotlin idiomatic

--- src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/AlternativeProductsService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/AlternativeProductsService.kt
@@ -2,6 +2,7 @@ package no.nav.hm.grunndata.alternativprodukter
 
 import jakarta.inject.Singleton
 import org.slf4j.LoggerFactory
+import java.util.UUID
 
 @Singleton
 open class AlternativeProductsService(
@@ -12,58 +13,36 @@ open class AlternativeProductsService(
         private val LOG = LoggerFactory.getLogger(AlternativeProductsService::class.java)
     }
 
-    fun getAlternativeProducts(hmsArtnr: String): List<String> {
-        return hmsArtnrMappingRepository.findBySourceHmsArtnr(hmsArtnr).map { it.targetHmsArtnr }
-    }
+    fun getAlternativeProducts(hmsArtnr: String): List<String> =
+        hmsArtnrMappingRepository.findBySourceHmsArtnr(hmsArtnr).map { it.targetHmsArtnr }
 
-    private fun generateMappingList(hmsArtnrList: List<String>): List<Pair<String, String>> {
-        val pairs = mutableListOf<Pair<String, String>>()
-        for (i in hmsArtnrList.indices) {
-            for (j in hmsArtnrList.indices) {
-                if (i != j) {
-                    pairs.add(Pair(hmsArtnrList[i], hmsArtnrList[j]))
-                }
+    private fun generateMappingList(hmsArtnrList: List<String>): List<Pair<String, String>> =
+        hmsArtnrList.indices.flatMap { i ->
+            hmsArtnrList.indices.mapNotNull { j ->
+                if (i != j) Pair(
+                    hmsArtnrList[i],
+                    hmsArtnrList[j]
+                ) else null
             }
         }
-
-        return pairs
-    }
 
     fun deleteAlternativeProducts(hmsArtnrList: List<String>) {
-        if (hmsArtnrList.size < 2) {
-            throw IllegalArgumentException("List must contain at least two elements for there to be a mapping")
-        }
+        require(hmsArtnrList.size >= 2) { "List must contain at least two elements for there to be a mapping" }
 
-        val pairs = generateMappingList(hmsArtnrList)
-
-        pairs.map {
-            val existingMapping = hmsArtnrMappingRepository.findBySourceHmsArtnrAndTargetHmsArtnr(it.first, it.second)
-            if (existingMapping != null) {
-                hmsArtnrMappingRepository.deleteMapping(sourceHmsArtnr = it.first, targetHmsArtnr = it.second)
-            } else {
-                LOG.warn("Did not find mapping to delete for sourceHmsArtnr: ${it.first} and targetHmsArtnr: ${it.second}")
-            }
-
+        generateMappingList(hmsArtnrList).forEach { (first, second) ->
+            hmsArtnrMappingRepository.findBySourceHmsArtnrAndTargetHmsArtnr(first, second)?.let {
+                hmsArtnrMappingRepository.deleteMapping(first, second)
+            } ?: LOG.warn("Did not find mapping to delete for sourceHmsArtnr: $first and targetHmsArtnr: $second")
         }
     }
 
-    suspend fun saveAlternativeProducts(hmsArtnrList: List<String>) {
-        if (hmsArtnrList.size < 2) {
-            throw IllegalArgumentException("List must contain at least two elements for there to be a mapping")
+    fun saveAlternativeProducts(hmsArtnrList: List<String>) {
+        require(hmsArtnrList.size >= 2) { "List must contain at least two elements for there to be a mapping" }
+
+        generateMappingList(hmsArtnrList).forEach { (first, second) ->
+            hmsArtnrMappingRepository.findBySourceHmsArtnrAndTargetHmsArtnr(first, second)?.let {
+                LOG.warn("Mapping already exists for sourceHmsArtnr: $first and targetHmsArtnr: $second")
+            } ?: hmsArtnrMappingRepository.insertMapping(UUID.randomUUID(), first, second)
         }
-
-        val pairs = generateMappingList(hmsArtnrList)
-
-        pairs.map {
-            val existingMapping = hmsArtnrMappingRepository.findBySourceHmsArtnrAndTargetHmsArtnr(it.first, it.second)
-            if (existingMapping != null) {
-                LOG.warn("Mapping already exists for sourceHmsArtnr: ${it.first} and targetHmsArtnr: ${it.second}")
-            } else {
-                hmsArtnrMappingRepository.save(HmsArtnrMapping(sourceHmsArtnr = it.first, targetHmsArtnr = it.second))
-            }
-
-        }
-
     }
-
 }
