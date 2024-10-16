4bececf63b08557b035d79e0e985dc2075478bb8
Atle Nordland <atle.nordland@nav.no>
2024-10-16 13:15:19 +0200

    fiks url

--- src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/OebsClient.kt
+++ src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/OebsClient.kt
@@ -1,12 +1,11 @@
 package no.nav.hm.grunndata.alternativprodukter
 
-import io.micronaut.core.annotation.Introspected
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Header
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.serde.annotation.Serdeable
 
-@Client("https://hm-oebs-api-proxy.dev-fss-pub.nais.io")
+@Client("\${oebs.url}")
 interface OebsClient {
 
     @Get(value = "/lager/alle-sentraler/{hmsNr}")
