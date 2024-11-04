e7a96d2ea45f04569119f6c6264ad5f7301ca49a
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-11-04 12:20:06 +0100

    La til stoler med oppreisingsfunksjon

--- src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/importing/SubstituteFiles.kt
+++ src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/importing/SubstituteFiles.kt
@@ -2,5 +2,6 @@ package no.nav.hm.grunndata.alternativprodukter.importing
 
 enum class SubstituteFiles(val fileName: String) {
     V1_PERSONLOFTERE("V1_personloftere.xlsx"),
-    V2_PERSONLOFTERE("V2_personloftere.xlsx");
+    V2_PERSONLOFTERE("V2_personloftere.xlsx"),
+    V3_STOLER_MED_OPPREISINGSFUNKSJON("V3_stoler_med_oppreisingsfunksjon.xlsx");
 }
