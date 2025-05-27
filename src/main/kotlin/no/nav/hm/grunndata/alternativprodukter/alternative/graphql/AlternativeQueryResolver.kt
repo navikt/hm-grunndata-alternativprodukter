1bd7aa4003010e3740713fa45c2a6f8b5a8756f9
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-05-27 13:56:32 +0200

    use searchService instead of searchApi

--- src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/alternative/graphql/AlternativeQueryResolver.kt
+++ src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/alternative/graphql/AlternativeQueryResolver.kt
@@ -6,16 +6,15 @@ import jakarta.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import no.nav.hm.grunndata.alternativprodukter.alternative.SearchApi
+import no.nav.hm.grunndata.alternativprodukter.alternative.SearchService
+import no.nav.hm.grunndata.alternativprodukter.alternative.SearchService.Companion.ALTERNATIVES
 import no.nav.hm.grunndata.alternativprodukter.index.AlternativeProductDoc
 import no.nav.hm.grunndata.alternativprodukter.index.AlternativeProductIndexer
-import no.nav.hm.grunndata.alternativprodukter.stock.ProductStock
 import no.nav.hm.grunndata.alternativprodukter.stock.ProductStockDTO
 import no.nav.hm.grunndata.alternativprodukter.stock.ProductStockService
-import no.nav.hm.grunndata.alternativprodukter.stock.WarehouseStock
 
 @Singleton
-class AlternativeQueryResolver(private val searchApi: SearchApi,
+class AlternativeQueryResolver(private val searchService: SearchService,
                                private val productStockService: ProductStockService,
                                private val indexer: AlternativeProductIndexer,
                                private val objectMapper: ObjectMapper) {
@@ -25,15 +24,10 @@ class AlternativeQueryResolver(private val searchApi: SearchApi,
     fun searchAlternativeProducts(hmsNrs: List<String>): List<AlternativeProductDoc> {
         val body = buildQueryBody(hmsNrs)
         LOG.debug("Query: $body")
-        val response = searchApi.searchAlternativesWithBody(null, body)
-        if (response.status() == HttpStatus.OK) {
-            val jsonString = response.body.get()
-            val json =  objectMapper.readTree(jsonString)
-            LOG.debug(jsonString)
-            val hits = json.get("hits").get("hits")
-            return hits.map { objectMapper.treeToValue(it.get("_source"), AlternativeProductDoc::class.java)  }
-        }
-        return emptyList()
+        val response = searchService.searchWithBody(index = ALTERNATIVES, params = emptyMap(), body = body )
+        val json =  objectMapper.readTree(response)
+        val hits = json.get("hits").get("hits")
+        return hits.map { objectMapper.treeToValue(it.get("_source"), AlternativeProductDoc::class.java)  }
     }
 
     fun getProductStock(hmsnr: String): ProductStockDTO {
