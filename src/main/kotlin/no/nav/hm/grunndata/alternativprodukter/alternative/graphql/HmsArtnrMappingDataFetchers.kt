b5597deff543f0e8b060f4989df0ae3f1524b5ee
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-06-11 15:10:48 +0200

    more logs when call graphql

--- src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/alternative/graphql/HmsArtnrMappingDataFetchers.kt
+++ src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/alternative/graphql/HmsArtnrMappingDataFetchers.kt
@@ -2,16 +2,20 @@ package no.nav.hm.grunndata.alternativprodukter.alternative.graphql
 
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
+import io.micronaut.data.model.query.factory.Projections.id
 import jakarta.inject.Singleton
 import no.nav.hm.grunndata.alternativprodukter.alternative.HmsArtnrMapping
 import no.nav.hm.grunndata.alternativprodukter.alternative.HmsArtnrMappingInputDTO
+import org.slf4j.LoggerFactory
 import java.util.UUID
 
 @Singleton
 class HmsArtnrMappingDataFetchers(private val hmsArtnrMappingResolver: HmsArtnrMappingResolver) {
 
+
     fun createHmsArtnrMappingDataFetcher(): DataFetcher<HmsArtnrMapping> {
         return DataFetcher { env: DataFetchingEnvironment ->
+            LOG.info("creating HmsArtnrMapping")
             val input = env.getArgument<HmsArtnrMappingInputDTO>("input")
             hmsArtnrMappingResolver.createHmsArtnrMapping(input!!)
         }
@@ -20,6 +24,7 @@ class HmsArtnrMappingDataFetchers(private val hmsArtnrMappingResolver: HmsArtnrM
     fun updateHmsArtnrMappingDataFetcher() : DataFetcher<HmsArtnrMapping> {
         return DataFetcher { env: DataFetchingEnvironment ->
             val id = env.getArgument<UUID>("id")
+            LOG.info("updating HmsArtnrMapping for $id")
             val input = env.getArgument<HmsArtnrMappingInputDTO>("input")
             hmsArtnrMappingResolver.updateHmsArtnrMapping(id!!, input!!)
         }
@@ -28,6 +33,7 @@ class HmsArtnrMappingDataFetchers(private val hmsArtnrMappingResolver: HmsArtnrM
     fun deleteHmsArtnrMappingDataFetcher() : DataFetcher<Boolean> {
         return DataFetcher { env: DataFetchingEnvironment ->
             val id = env.getArgument<UUID>("id")
+            LOG.info("deleting HmsArtnrMapping for $id")
             ( hmsArtnrMappingResolver.deleteHmsArtnrMapping(id!!)  == 1 )
         }
     }
@@ -35,6 +41,7 @@ class HmsArtnrMappingDataFetchers(private val hmsArtnrMappingResolver: HmsArtnrM
     fun getHmsArtnrMappingById() : DataFetcher<HmsArtnrMapping> {
         return DataFetcher { env ->
             val id = env.getArgument<UUID>("id")
+            LOG.info("getting HmsArtnrMapping for $id")
             hmsArtnrMappingResolver.getHmsArtnrMappingById(id!!)
         }
     }
@@ -42,8 +49,12 @@ class HmsArtnrMappingDataFetchers(private val hmsArtnrMappingResolver: HmsArtnrM
     fun getHmsArtnrMappingBySourceHmsArtnr(): DataFetcher<List<HmsArtnrMapping>> {
         return DataFetcher { env ->
             val sourceHmsArtnr = env.getArgument<String>("sourceHmsArtnr")
+            LOG.info("getting HmsArtnrMapping for $sourceHmsArtnr")
             hmsArtnrMappingResolver.getHmsArtnrMappingBySourceHmsArtnr(sourceHmsArtnr!!)
         }
     }
 
+    companion object {
+        private val LOG = LoggerFactory.getLogger(HmsArtnrMappingDataFetchers::class.java)
+    }
 }
