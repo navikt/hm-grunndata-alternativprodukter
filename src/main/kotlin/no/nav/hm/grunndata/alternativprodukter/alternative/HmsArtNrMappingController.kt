c7b76dcba18f61085774e0affe3406d7c5f52e73
marte pedersen <marte.pedersen@nav.no>
2025-10-29 10:59:42 +0100

    fjerner ubrukte endepoint

--- src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/alternative/HmsArtNrMappingController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/alternativprodukter/alternative/HmsArtNrMappingController.kt
@@ -14,33 +14,12 @@ class HmsArtNrMappingController(private val hmsArtnrMappingRepository: HmsArtnrM
     @Get("/all/distinct")
     suspend fun getAllDistinctHmsArtnr() = hmsArtnrMappingRepository.findDistinctSourceHmsArtnr()
 
-    @Post("/create")
-    suspend fun createMapping(
-        @Body mapping: HmsArtnrMappingInputDTO
-    ): List<HmsArtnrMappingInputDTO> {
-        val createdMapping = hmsArtnrMappingRepository.save(
-            HmsArtnrMapping(
-                sourceHmsArtnr = mapping.sourceHmsArtnr,
-                targetHmsArtnr = mapping.targetHmsArtnr
-            )
-        )
-
-        val createdReverseMapping = hmsArtnrMappingRepository.save(
-            HmsArtnrMapping(
-                sourceHmsArtnr = mapping.targetHmsArtnr,
-                targetHmsArtnr = mapping.sourceHmsArtnr
-            )
-        )
-
-        return listOf(createdMapping.toDto(), createdReverseMapping.toDto())
-    }
-
     @Post("/group/add")
     suspend fun addToGroup(
         @Body editGroupDTO: EditGroupDTO
     ): HttpResponse<List<HmsArtnrMappingInputDTO>> {
 
-        if(editGroupDTO.group.contains(editGroupDTO.alternative)) {
+        if (editGroupDTO.group.contains(editGroupDTO.alternative)) {
             return HttpResponse.badRequest()
         }
 
@@ -78,23 +57,6 @@ class HmsArtNrMappingController(private val hmsArtnrMappingRepository: HmsArtnrM
             )
         }
     }
-
-    @Delete("/delete")
-    suspend fun deleteMapping(
-        @Body mapping: HmsArtnrMappingInputDTO
-    ) {
-        hmsArtnrMappingRepository.deleteMapping(
-            sourceHmsArtnr = mapping.sourceHmsArtnr,
-            targetHmsArtnr = mapping.targetHmsArtnr
-        )
-
-        hmsArtnrMappingRepository.deleteMapping(
-            sourceHmsArtnr = mapping.targetHmsArtnr,
-            targetHmsArtnr = mapping.sourceHmsArtnr
-        )
-    }
-
-
 }
 
 @Serdeable
